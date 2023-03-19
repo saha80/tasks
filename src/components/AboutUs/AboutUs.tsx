03153bcbd9f1eb2e6abc8819188d6cd53546469f
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-03-20 00:55:24 +0200

    feat: add AboutUs content

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -4,6 +4,15 @@ import './AboutUs.css';
 
 export class AboutUs extends Component {
   render() {
-    return <div className="about-us">About Us</div>;
+    return (
+      <div className="about-us">
+        <h1>About Us</h1>
+        <p>
+          RS School is free-of-charge and community-based education program conducted by{' '}
+          <a href="https://rollingscopes.com/">The Rolling Scopes</a> developer community since
+          2013.
+        </p>
+      </div>
+    );
   }
 }
