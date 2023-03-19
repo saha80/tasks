1e39af3231a61d547ab20f17a44e9618882fc430
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-03-20 01:33:02 +0200

    feat: remove unused .md files, add gitignore

--- README.md
+++
@@ -1,6 +0,0 @@
-# Fetch .md files from rs school
-
-```bash
-curl --url https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/react/modules/moduleYOUR_MODULE_NUMBER/YOUR_FILE_NAME.md --get --output YOUR_FILE_NAME.md
-
-```
--- sync-MDs.ps1
+++
@@ -1,2 +0,0 @@
-curl --url https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/react/modules/module01/README.md --get --output README.md
-curl --url https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/react/modules/module01/configs.md --get --output configs.md
