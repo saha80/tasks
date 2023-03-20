c147adf0b1897cf3921a6be83550e3d01762ff5b
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-03-20 12:36:04 +0200

    fix: PageNotFound styles

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,12 @@
 import { Component } from 'react';
 
+import './PageNotFound.css';
+
 export class PageNotFound extends Component {
   render() {
     return (
       <div className="page-not-found">
-        <span>404 Page Not Found</span>
+        <h1>404 Page Not Found</h1>
         <a href="/">Go to Home</a>
       </div>
     );
