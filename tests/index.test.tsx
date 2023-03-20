de832ff7b95d0cb362467d50ac482faff1dbc639
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-03-20 12:36:56 +0200

    feat: add tests

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,18 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import { RouterProvider } from 'react-router-dom';
 
+import { AboutUs } from '../src/components/AboutUs/AboutUs';
+import { Card } from '../src/components/Card/Card';
 import { Cards } from '../src/components/Cards/Cards';
 import { router } from '../src/pages/Routes';
 
+import { getCards } from '../src/service/Card';
+
 describe('App', () => {
-  it('renders', () => {
+  test('renders', () => {
     render(<RouterProvider router={router} />);
 
     screen.debug();
   });
 
-  it('Cards', () => {
+  test('Cards', () => {
     render(<Cards />);
+
+    screen.debug();
+  });
+
+  test('Card', async () => {
+    for (const card of await getCards()) {
+      render(<Card {...card} />);
+    }
+
+    screen.debug();
+  });
+
+  test('AboutUs', () => {
+    render(<AboutUs />);
 
     screen.debug();
   });
