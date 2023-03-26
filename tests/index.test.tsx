4805483a6c836330065aa3a770f11cecc86376aa
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-03-26 21:29:20 +0300

    refactor: move mock data to separate file

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -4,102 +4,34 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from '@/routes/Routes';
 import { AboutUs } from '@/pages/AboutUs/AboutUs';
 
-import { Card, CardProps } from '@/components/Card/Card';
-import { Cards, CardsProps } from '@/components/Cards/Cards';
-
-// todo: move to separate file
-const card: CardProps = {
-  id: 1,
-  src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
-  createdBy: 'Alex',
-  creationTimestamp: Date.now(),
-  modificationTimestamp: Date.now(),
-  title: 'Awesome card about React',
-  description: 'Very long card description about created card',
-  topics: ['programming'],
-  tags: ['react', 'card', 'first card'],
-  visibility: 'only-you',
-  likes: 10,
-  views: 10,
-};
-
-const cards: CardsProps = {
-  cards: [
-    card,
-    {
-      id: 2,
-      src: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png',
-      createdBy: 'Alex',
-      creationTimestamp: Date.now(),
-      modificationTimestamp: Date.now(),
-      title: 'Awesome card about Redux',
-      description: 'Very long card description about created card',
-      topics: ['programming'],
-      tags: ['Redux', 'card'],
-      visibility: 'only-you',
-      likes: 10,
-      views: 10,
-    },
-    {
-      id: 3,
-      src: 'https://www.typescriptlang.org/images/branding/two-longform.svg',
-      createdBy: 'Alex',
-      creationTimestamp: Date.now(),
-      modificationTimestamp: Date.now(),
-      title: 'Awesome card about Typescript',
-      description: 'Very long card description about created card',
-      topics: ['programming'],
-      tags: ['Typescript', 'card'],
-      visibility: 'only-you',
-      likes: 10,
-      views: 10,
-    },
-    {
-      id: 4,
-      src: 'https://www.w3schools.com/css/img_forest.jpg',
-      createdBy: 'Alex',
-      creationTimestamp: Date.now(),
-      modificationTimestamp: Date.now(),
-      title: 'Card about forest',
-      description: 'Very long card description about created card',
-      topics: ['travelling'],
-      tags: ['forest', 'card', 'nature'],
-      visibility: 'only-you',
-      likes: 10,
-      views: 10,
-    },
-    {
-      id: 5,
-      src: 'https://www.w3schools.com/css/img_mountains.jpg',
-      createdBy: 'Alex',
-      creationTimestamp: Date.now(),
-      modificationTimestamp: Date.now(),
-      title: 'Card about Mountains',
-      description: 'Very long card description about created card',
-      topics: ['travelling'],
-      tags: ['mountains', 'card', 'nature'],
-      visibility: 'only-you',
-      likes: 10,
-      views: 10,
-    },
-  ],
-};
+import { Card } from '@/components/Card/Card';
+import { Cards } from '@/components/Cards/Cards';
+import { card, cards } from './mockData';
 
 describe('App', () => {
-  test('renders', () => {
+  test('renders', async () => {
     render(<RouterProvider router={router} />);
+
+    const [home] = await screen.findAllByText('Home');
+    expect(home.innerHTML).toEqual('Home');
 
     screen.debug();
   });
 
-  test('Cards', () => {
+  test('Cards', async () => {
     render(<Card {...card} />);
+
+    const [renderedCard] = await screen.findAllByText(card.title);
+    expect(renderedCard.innerHTML).toEqual(card.title);
 
     screen.debug();
   });
 
   test('Card', async () => {
     render(<Cards {...cards} />);
+
+    const [renderedCard] = await screen.findAllByText(cards.cards[2].title);
+    expect(renderedCard.innerHTML).toEqual(cards.cards[2].title);
 
     screen.debug();
   });
