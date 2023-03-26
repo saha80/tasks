f20dba98c580f89eeda29aaec102f8e9f4356119
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-03-26 21:01:06 +0300

    refactor: rename Fieldset to RadioGroup

---
+++ src/components/Input/components/RadioGroup.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, forwardRef } from 'react';
+
+import { FormInput } from './interfaces';
+import { Label } from './Label';
+
+interface RadioGroupProps extends FormInput {
+  legend: ReactNode;
+  children: Array<string>;
+}
+
+export const RadioGroup = forwardRef<HTMLInputElement, RadioGroupProps>(
+  ({ legend, children, disabled = false, className, form, name }, ref) => (
+    <fieldset disabled={disabled} className="fieldset">
+      <legend className="input-legend">{legend}</legend>
+      {children.map((value, index) => (
+        <div key={index} className="group">
+          <Label>
+            {
+              <input
+                className={className}
+                form={form}
+                name={name}
+                type="radio"
+                value={value}
+                required
+                ref={ref}
+              />
+            }
+            {value}
+          </Label>
+        </div>
+      ))}
+    </fieldset>
+  )
+);
