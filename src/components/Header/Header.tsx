c11678018a4e86b942753d82d91934e92c35c4ff
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-03-31 11:09:16 +0300

    feat: use hooks in Header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,20 @@
 import { ROOT_CHILDREN } from '@/routes/Routes';
-import { withRouter } from '@/hoc/withRouter';
+import { useLocation } from 'react-router-dom';
 
 import { Nav } from './components/Nav';
 import { pathToLinkName } from './components/Link';
 
 import './Header.css';
 
-export const Header = withRouter(({ location }) => (
-  <header className="header">
-    <Nav routes={ROOT_CHILDREN} />
-    {location && (
+export const Header = () => {
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <Nav routes={ROOT_CHILDREN} />
       <h4 className="current-path">
         Current path: {pathToLinkName(location.pathname)}
       </h4>
-    )}
-  </header>
-));
+    </header>
+  );
+};
