7dd716ed68608b4211dadf1a7aa889939ce4be9a
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-04-10 02:15:18 +0300

    feat: add cardListDetailsWithService HOC

---
+++ src/hoc/cardListWithService.tsx
@@ -0,0 +1,56 @@
+import { FC, useContext, useEffect, useState } from 'react';
+
+import { FetchingProgress, FetchingError, CardListProps } from '@/components';
+import { CardService } from '@/interfaces/CardService';
+import { Card } from '@/interfaces/Card';
+import { CardsContext } from '@/context/CardsContext';
+
+export type CardListWithServiceProps = Omit<CardListProps, 'children'>;
+
+export const cardListWithService = (
+  Wrapped: React.ComponentType<CardListProps>,
+  cardService: CardService
+) => {
+  const CardListWithService: FC<CardListWithServiceProps> = (props) => {
+    const { searchValue } = useContext(CardsContext);
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | undefined>(undefined);
+    const [cardList, setCardList] = useState<Card[] | undefined>(undefined);
+
+    useEffect(() => {
+      const abortController = new AbortController();
+
+      (searchValue
+        ? cardService.getCardListByQuery(abortController.signal, {
+            query: searchValue,
+          })
+        : cardService.getCardList(abortController.signal)
+      )
+        .then(setCardList)
+        .catch((err: Error) => {
+          if (err instanceof DOMException && err.name === 'AbortError') {
+            return;
+          }
+          setError(err);
+        })
+        .finally(() => setLoading(false));
+
+      return () => {
+        abortController.abort();
+      };
+    }, [searchValue]);
+
+    if (loading || !cardList) {
+      return <FetchingProgress label="Progressing..." />;
+    }
+
+    if (error) {
+      return <FetchingError />;
+    }
+
+    return <Wrapped {...props}>{cardList}</Wrapped>;
+  };
+
+  return CardListWithService;
+};
