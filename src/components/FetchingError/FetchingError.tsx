cdc739774aeaff6836c60feca571b500f3531343
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-04-10 01:44:04 +0300

    feat: add FetchingError

--- src/components/FetchingError/FetchingError.tsx
+++ src/components/FetchingError/FetchingError.tsx
@@ -1,5 +1,13 @@
 import { FC } from 'react';
 
-import './FetchingError.css';
+import styles from './FetchingError.module.css';
 
-export const FetchingError: FC = () => <div>Error</div>;
+export interface FetchingErrorProps {
+  message?: string;
+}
+
+const defaultMessage = 'Error! Something bad happend!';
+
+export const FetchingError: FC<FetchingErrorProps> = ({
+  message = defaultMessage,
+}) => <div className={styles.error}>{message}</div>;
