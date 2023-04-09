00beda6ddeea58ea0df54dd95ffd6f1ab89a3ad1
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-04-10 02:18:13 +0300

    feat: upgrade Search on Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { FC, useMemo, useState } from 'react';
+import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Header, Footer } from '@/components';
-import {
-  CardsContext,
-  CardsContextType,
-  initialCardsContextValue,
-} from '@/context/CardsContext';
 
 import styles from './App.module.css';
 
-export const App: FC = () => {
-  const [searchValue, setSearchValue] = useState<string | null>(null);
-
-  const value = useMemo<CardsContextType>(
-    () => ({
-      ...initialCardsContextValue,
-      searchValue,
-      onChange: setSearchValue,
-    }),
-    [searchValue]
-  );
-
-  return (
-    <CardsContext.Provider value={value}>
-      <Header />
-      <main className={styles.app}>
-        <Outlet />
-      </main>
-      <Footer />
-    </CardsContext.Provider>
-  );
-};
+export const App: FC = () => (
+  <>
+    <Header />
+    <main className={styles.app}>
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+);
